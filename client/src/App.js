80ecf0d940b7753e891a51bb8e6c3da4eb0047c1
vzaleto <vzaleto@gmail.com>
2025-07-10 19:19:39 +0300

    update client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import axios from "axios";
 
 function App() {
 
     const [url, setUrl] = useState('');
     const [genetrateLink, setGentraleLink] = useState('');
     const [error, setError] = useState('');
+    const [LocalData, setLocationData] = useState([]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -19,11 +21,30 @@ function App() {
 
         const encodedUrl = encodeURIComponent(url);
         const trackerUrl = `${window.location.origin}/go?to=${encodedUrl}`;
+        // const trackerUrl = `http://0.0.0.0:3001/go?to=${encodedUrl}`;
 
         setGentraleLink(trackerUrl);
       setError('')
 
     }
+    useEffect(  () => {
+
+        const fetchLogs = async () =>{
+            try{
+                const response = await axios.get('http://0.0.0.0:3001/log')
+
+                    setLocationData(response.data)
+
+            }catch (error) {
+                console.error('Error in fetchLogs:', error);
+            }
+
+        }
+        fetchLogs()
+
+    }, [LocalData]);
+
+    console.log(LocalData)
 
     return (
         <div>
@@ -41,6 +62,20 @@ function App() {
                     </div>
                 )
             }
+            <div>
+                <h2>Логи</h2>
+                {LocalData.length > 0 && LocalData.map((loc, index) => (
+                    <div key={index}>
+                        <p>Дата: {loc.timestamp}</p>
+                        <p>IP: {loc.ip}</p>
+                        <p>status :{loc.location.status}</p>
+                        <p>City :{loc.location.city}</p>
+                        <p>Lat : {loc.location.lat}</p>
+                        <p>lon : {loc.location.lon}</p>
+                        <p>provider : {loc.location.isp}</p>
+                    </div>
+                ))}
+            </div>
         </div>
 );
 
--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3001;
+const cors = require('cors');
 
-
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+const LocalData = [];
 
 app.get('/go', async (req, res) => {
 
@@ -13,47 +16,50 @@ app.get('/go', async (req, res) => {
     if (!to) {
         return res.status(400).send({error: 'Missing to parameter'});
     }
-
-
-
     // проверка URL
     try {
         new URL(to);
     } catch (err) {
-        return res.status(400).send({ error: 'Invalid URL' });
+        return res.status(400).send({error: 'Invalid URL'});
     }
 
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || 'unknown';
+    const ipHeader = req.headers['x-forwarded-for'] || req.socket.remoteAddress || 'unknown';
+    const ip = ipHeader.split(',')[0];
     const ua = req.headers['user-agent'] || 'unknown';
 
     let geo = {};
 
+
     try {
         geo = await fetch(`http://ip-api.com/json/${ip}`).then(res => res.json());
     } catch (err) {
-        geo = { error: err.message || 'Could not fetch geo' };
+        geo = {error: err.message || 'Could not fetch geo'};
         // geo = {error: err.message};
     }
 
     const log = {
         timestamp: new Date().toISOString(),
-        ip,
+        ip:ipHeader,
         userAgent: ua,
         location: geo
     }
-
+    LocalData.push(log);
     console.log(JSON.stringify(log, null, 2));
-
-    console.log({ to });
-
-    res.set('ngrok-skip-browser-warning', 'true');
     res.redirect(to);
 })
+app.get('/log', (req, res) => {
+    if(LocalData.length){
+        res.json(LocalData)
+    }else{
+        res.json({Message: 'LocalData is empty'})
+    }
 
+})
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
-app.listen(PORT,'0.0.0.0', () => {
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server listening on port 'http://0.0.0.0:${PORT}'`);
 })
